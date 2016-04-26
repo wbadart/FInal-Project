b7c74c094e672d50d5dbb540323f0049960a4162
Daniel Wilborn <dwilborn@fgr.dhcp.nd.edu>
2016-04-25 23:37:38 -0400

    Bone Class

---
+++ Bone.h
@@ -0,0 +1,26 @@
+#ifndef BONE_H
+#define BONE_H
+
+#include "Object.h"
+
+class Bone: public Object {
+
+	public:
+		Bone();
+
+	private:
+		
+
+
+
+
+
+}
+
+Bone::Bone(): Object(5) {
+
+
+}
+
+
+#endif
