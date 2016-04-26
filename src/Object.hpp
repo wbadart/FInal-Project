a49d68810d22e88b3edd458a4bb3878e0eccf744
Catherine Badart <cbadart@badarty427.dhcp.nd.edu>
2016-04-26 00:06:03 -0400

    added a few member functions to Object class

--- src/Object.hpp
+++ src/Object.hpp
@@ -7,11 +7,12 @@ using namespace std;
 class Object {
 
 	public:
-		
 		Object(int);
 		~Object();
-		int getPoints();		
-
+		int getPoints();
+		void setPoints(int);	//can either add or subtract points
+		int isCollision();	//returns 1 if there is a collision
+		virtual void draw();	//draw function
 	private:
 		int points;
 
@@ -32,9 +33,21 @@ Object::~Object() {
 int Object::getPoints() {
 
 	return points;
-	
 }
 
+void Object::setPoints(int p) {
+
+	points = p;
+}
+
+int Obeject::isCollision() {
+	//collision detection stuff
+	//maybe pass in an object as a parameter?
+}
+
+void Object::draw() {
+	//drawing
+}
 #endif
 
 
