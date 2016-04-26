d4eec8691f09f55f4137fd1420e6f06b12f07200
Daniel Wilborn <dwilborn@fgr.dhcp.nd.edu>
2016-04-25 23:38:25 -0400

    ChewToy Class

---
+++ ChewToy.h
@@ -0,0 +1,26 @@
+#ifndef ChewToy_H
+#define ChewToy_H
+
+#include "Object.h"
+
+class ChewToy: public Object {
+
+	public:
+		ChewToy();
+
+	private:
+		
+
+
+
+
+
+}
+
+ChewToy::ChewToy(): Object(10) {
+
+
+}
+
+
+#endif
